9eec9822437aa122dfbab593e4b7e66aa6cfd603
George <killyen444@gmail.com>
2024-04-02 12:54:06 -0700

    updated header

--- src/conponents/header/Header.js
+++ src/conponents/header/Header.js
@@ -1,11 +1,37 @@
 import "./header.css";
+import { LoremIpsum } from 'lorem-ipsum';
 
+const Header = () => {
 
+    // Create an instance of the LoremIpsum class
+     const lorem = new LoremIpsum();
+    // Generate 25 "words" of Lorem Ipsum text
+    const loremText = lorem.generateWords(25);
 
-const Header = () =>{
     return (
-        <div className = "header"> Header </div>
-            
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem">
+                        {/* <div className ="headerListItem">
+
+                        </div> */}
+
+
+
+                    </div>
+                </div>
+
+
+
+                <h1 className ="headerTitle">Start to find your amazing homestay</h1>
+                <p className="headerDesc">
+                        {loremText}
+                </p>
+                <button className="headerButton">Sign in / Register</button>
+            </div>
+        </div>
+   
     )
 }
 
--- src/conponents/navbar/Navbar.js
+++ src/conponents/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import "./navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faGlobe  } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 
 const Navbar = () => {
@@ -9,34 +9,35 @@ const Navbar = () => {
 
 
     return (
-        <div className = "navbar">
-            <div className = "navContainer">
-                <span className = "navLogo">BookMyHomestay</span>
-                <div className = "navMenu">
+        <div className="navbar">
+            <div className="navContainer">
+                <span className="navLogo">BookMyHomestay</span>
+                <div className="navMenu">
                     {/* <button className="navButton">Register</button> */}
                     <button className="navButton">Login</button>
 
+
+                    {/* implement favorite list logic here */}
+                    <div className="favoriteList">
+                        <FontAwesomeIcon icon={faHeart} className="favoriteIcon" />
+                        <span className="favoriteCount">0</span>
+                    </div>
                     {/* implement multiple language logic here */}
                     <div className="languageSelection">
                         <FontAwesomeIcon icon={faGlobe} className="languageIcon" onClick={() => setShowDropdown(!showDropdown)} />
                         {showDropdown && (
                             <div className="languageDropdown">
-                                <div className="languageOption" onClick={() => alert('English Selected')}>English</div>
+                                <div className="languageOption" onClick={() => alert('English Selected')}>Korean</div>
                                 <div className="languageOption" onClick={() => alert('Chinese Selected')}>Chinese</div>
                                 <div className="languageOption" onClick={() => alert('Brazilian Selected')}>Brazilian</div>
                             </div>
                         )}
                     </div>
-                    {/* implement favorite list logic here */}
-                    <div className="favoriteList">
-                        <FontAwesomeIcon icon={faHeart} className="favoriteIcon"/>
-                        <span className="favoriteCount">0</span> 
-                    </div>
-
                     {/*Welcome message*/}
                     <div className="welcomeMessage">Welcome, Guest</div>
-                    
+
                 </div>
+
             </div>
         </div>
     )
